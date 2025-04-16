40772e6051e5fe5b0f5f277a498199e65cc00d66
Yago Moreira <yagomoreira_dev@yahoo.com>
2025-04-16 12:52:16 -0300

    .

--- src/main/java/br/gov/ce/direitoshumanos/api_agenda/utils/VerificaSenhas.java
+++ src/main/java/br/gov/ce/direitoshumanos/api_agenda/utils/VerificaSenhas.java
@@ -6,8 +6,8 @@ public class VerificaSenhas {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-        String cpf = "055.548.143-36"; // CPF do ID 128
-        String hash = "$2a$10$S90adlsEZZf1YG0eg.TpUunpKMmuF9Q69/4tpNthaOi99apwCLjSS"; // senha do ID 128
+        String cpf = "123.456.789-00"; // CPF do ID 128
+        String hash = "$2a$10$xiP5ZyvWA4Nxbx28kIIyWunL9dEMxSGTf4nLbkUegCwCxGFWbtvKa"; // senha do ID 128
 
         boolean corresponde = encoder.matches(cpf, hash);
         System.out.println("CPF confere com hash? " + corresponde);
